e9e9740254ac1c3a7af3bf1068c2a6a722d2a4cc
elbraulio <elbraulio274@gmail.com>
2022-10-14 21:59:17 -0300

    #4 refactor listeners

---
+++ anilang-parser/src/main/java/com/anilang/parser/InternalBaseErrorListener.java
@@ -0,0 +1,31 @@
+/*
+ * Property of Opencore
+ */
+
+package com.anilang.parser;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+/**
+ * Error listener that throws and exception if a parser error is found.
+ *
+ * @since 0.1.0
+ */
+final class InternalBaseErrorListener extends BaseErrorListener {
+
+    // @checkstyle ParameterNumberCheck (10 lines)
+    @Override
+    public void syntaxError(
+        final Recognizer<?, ?> recognizer,
+        final Object symbol,
+        final int line,
+        final int column,
+        final String msg,
+        final RecognitionException exception
+    ) {
+        final String template = "Failed to parse at [Line:Column] = [%s:%s] caused by %s";
+        throw new IllegalStateException(String.format(template, line, column, msg), exception);
+    }
+}
--- anilang-parser/src/main/java/com/anilang/parser/UncheckedAniParser.java
+++ anilang-parser/src/main/java/com/anilang/parser/UncheckedAniParser.java
@@ -6,9 +6,6 @@ package com.anilang.parser;
 import com.anilang.parser.antlr.AniParser;
 import java.io.IOException;
 import java.io.InputStream;
-import org.antlr.v4.runtime.BaseErrorListener;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
 
 /**
  * Read the source and create a parser without checking for {@link IOException}.
@@ -39,27 +36,5 @@ final class UncheckedAniParser implements IoCheckedSupplier<AniParser> {
             ),
             new InternalBaseErrorListener()
         ).get();
-    }
-
-    /**
-     * Error listener that throws and exception if a parser error is found.
-     *
-     * @since 0.1.0
-     */
-    private static final class InternalBaseErrorListener extends BaseErrorListener {
-
-        // @checkstyle ParameterNumberCheck (10 lines)
-        @Override
-        public void syntaxError(
-            final Recognizer<?, ?> recognizer,
-            final Object symbol,
-            final int line,
-            final int column,
-            final String msg,
-            final RecognitionException exception
-        ) {
-            final String template = "Failed to parse at [Line:Column] = [%s:%s] caused by %s";
-            throw new IllegalStateException(String.format(template, line, column, msg), exception);
-        }
     }
 }
